ba15ecf45aebd7507e5e9ef545f0b8994a408c88
Ahmed <iaahmed40@gmail.com>
2024-03-01 21:50:50 -0600

    update instructions

--- src/index.ts
+++ src/index.ts
@@ -64,32 +64,45 @@ export function applyDailyChanges(player: Player) {
 
 /*
 
-for every player:
-  function getTransactions:
-    get transactions from google sheet
-      parse transactions from sheet and create transactions 
 
-    insert income and expense into transaction sorted by timeSubmitted
+on startup:
+  for every player:
+    function getTransactions:
+      get transactions from google sheet
+        parse transactions from sheet and create transactions 
 
-  function processTransactions:
-    apply each transaction
-      we need to mark transacton as success or fail
-        if success, remove from pendingTransactions
-        if fail 
+      insert income and expense into transaction sorted by timeSubmitted
+
+    function processTransactions:
+      apply each transaction
+        we need to mark transacton as success or fail
+          if success, remove from pendingTransactions
+          if fail 
 
 
 Mustafa Ahmad TODOs:
 
 add population to city object
 
-create insertSorted function
+create insertSorted  - prefer binary search
 create dummy transactions list
 use insertSorted to insert income and expense into transactions
 
 switch from using switch statement to using if else ladder to process transactions
 process transactions in order of timeSubmitted
 
-support maneuver transaction;
+support maneuver transaction:
   move troops from one city you own to another city you own, major or minor
+
+support import and export transactions:
+  for a trade to be successful, 
+    both players must have the resources they are trading
+    both players must submit a compatibat and reciprocative transaction
+
+  ex trade. moskov trade with aurelian 100 gold for 10 iron
+
+  moskov request will contain: partner: aurelian, import: 10 iron, export: 100 gold
+  aurelian request will contain: partner: moskov, import: 100 gold, export: 10 iron
+
 
 */
